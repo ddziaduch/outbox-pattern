00a9eccfc971855dd3cc7f2088a8c8463b0360a0
Damian Dziaduch <damian@dziaduch.pl>
2022-11-27 11:23:01 +0100

    Tooling

--- tests/Unit/Application/EventDispatcherDecoratorTest.php
+++ tests/Unit/Application/EventDispatcherDecoratorTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ddziaduch\OutboxPattern\Tests\Unit\Application;
 
+use ddziaduch\OutboxPattern\Infrastructure\EventDispatcherDecorator;
+use ddziaduch\OutboxPattern\Infrastructure\EventsMemoryCache;
 use PHPUnit\Framework\TestCase;
 
 /** @covers \ddziaduch\OutboxPattern\Infrastructure\EventDispatcherDecorator */
@@ -11,6 +13,11 @@ class EventDispatcherDecoratorTest extends TestCase
 {
     public function testDispatching(): void
     {
-        // TODO
+        $cache = new EventsMemoryCache();
+        $dispatcher = new EventDispatcherDecorator($cache);
+        $event = new \stdClass();
+        $dispatcher->dispatch($event);
+        self::assertFalse($cache->isEmpty());
+        self::assertContains($event, $cache);
     }
 }
