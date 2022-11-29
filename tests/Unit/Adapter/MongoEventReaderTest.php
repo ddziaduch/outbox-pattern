b07b013f14db27604f3e68f40eb1a546ee3cb04b
Damian Dziaduch <damian@dziaduch.pl>
2022-11-29 21:06:19 +0100

    Fix test

--- tests/Unit/Adapter/MongoEventReaderTest.php
+++ tests/Unit/Adapter/MongoEventReaderTest.php
@@ -15,26 +15,30 @@ class MongoEventReaderTest extends TestCase
 {
     public function testReading(): void
     {
-        $objectFactory = static fn (object ...$outbox) => new class (...$outbox) {
-            /** @var object[] */
+        $objectFactory = static fn (int $numberOfEventsInOutbox): object => new class ($numberOfEventsInOutbox) {
+            /** @var string[] */
             public array $outbox;
 
-            public function __construct(object ...$outbox)
+            public function __construct(int $numberOfEventsInOutbox)
             {
-                $this->outbox = $outbox;
+                $this->outbox = array_fill(
+                    0,
+                    $numberOfEventsInOutbox,
+                    serialize(new \stdClass()),
+                );
             }
         };
 
-        $repositoryFactory = function (object ...$objects) {
+        $repositoryFactory = function (object ...$objects): ObjectRepository {
             $repository = $this->createStub(ObjectRepository::class);
             $repository->method('findBy')->willReturn($objects);
 
             return $repository;
         };
 
-        $object1 = $objectFactory(new \stdClass(), new \stdClass());
-        $object2 = $objectFactory(new \stdClass(), new \stdClass(), new \stdClass());
-        $object3 = $objectFactory(new \stdClass(), new \stdClass(), new \stdClass(), new \stdClass());
+        $object1 = $objectFactory(numberOfEventsInOutbox: 1);
+        $object2 = $objectFactory(numberOfEventsInOutbox: 2);
+        $object3 = $objectFactory(numberOfEventsInOutbox: 3);
 
         $repository1 = $repositoryFactory($object1, $object2);
         $repository2 = $repositoryFactory($object3);
@@ -43,13 +47,13 @@ class MongoEventReaderTest extends TestCase
         $repositories->method('getIterator')->willReturn(new \ArrayIterator([$repository1, $repository2]));
 
         $expectedEvents = [
-            ...$object1->outbox,
-            ...$object2->outbox,
-            ...$object3->outbox,
+            ...array_map('unserialize', $object1->outbox),
+            ...array_map('unserialize', $object2->outbox),
+            ...array_map('unserialize', $object3->outbox),
         ];
 
         $reader = new MongoEventReader($repositories, $this->createStub(ObjectManager::class));
         $actualEvents = [...$reader->read()];
-        self::assertSame($expectedEvents, $actualEvents);
+        self::assertEquals($expectedEvents, $actualEvents);
     }
 }
